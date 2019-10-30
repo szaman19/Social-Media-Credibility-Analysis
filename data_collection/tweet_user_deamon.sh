4c9bea37d4148f87ecd36b7a992829d9be4aa3ac
Shehtab Zaman <shehtabzaman@gmail.com>
2019-10-29 21:48:37 -0400

    Updated daemons

--- data_collection/tweet_user_deamon.sh
+++ data_collection/tweet_user_deamon.sh
@@ -2,7 +2,8 @@
 
 while true 
 do
-	python collect_user_data.py 2>user.err;
+	touch user.err
+	python twitter_user_data.py 2>user.err;
 	if [ -s user.err ]
 	then 
 		echo "Search request failed" | mail -s "Twitter Search Daemon" szaman5@binghamton.edu
