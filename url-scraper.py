d27187f042bbbd87f33a1e3e70f3157937775b18
Hayden Brown <hbrown10@cs.binghamton.edu>
2019-12-03 12:24:07 -0500

    simple script for pulling tweets with urls

---
+++ url-scraper.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+import time
+
+client = MongoClient()
+db = client['twitter_data_test']
+tweets = db['streamed_tweets_test']
+newCol = db['url_tweets_test']
+
+start = time.time()
+
+for tweet in tweets.find({"entities.urls": {'$not': {'$size': 0}}}):
+    newCol.insert_one(tweet)
+
+end = time.time() - start
+
+f = open("results.txt", "w+")
+f.write("Complete! Final count for newCol is ", newCol.count(), " tweets from the base collection's", tweets.count(), "\n")
+f.write("Total time elapsed: ", end, "\n")
