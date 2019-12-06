52fb847a28ccdbc67460c6ac20cd4a08a0500b42
Shehtab Zaman <szaman5@titan.cs.binghamton.edu>
2019-12-06 08:52:54 -0500

    Updated with new csv files

--- doc_to_csv.py
+++ doc_to_csv.py
@@ -4,10 +4,10 @@ import tldextract
 def main():
 	myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 	twitter_db = myclient["twitter_data"]
-	collection = twitter_db['url_tweets_search']
+	collection = twitter_db['url_tweets_test']
 	# print(collection.count())
 	f = open("domains_search.csv", 'w')
-	f.write("domains,suffix,subdomain \n")
+	f.write("domains,suffix,subdomain,reg_domain\n")
 	counter = 0
 	tweets = collection.find()
 	total  = tweets.count()
@@ -23,8 +23,12 @@ def main():
 					if(len(urls) > 0):
 						ext = tldextract.extract(urls[0]['expanded_url'])
 						f.write(ext.domain)
+						f.write(",")
 						f.write(ext.suffix)
+						f.write(",")
 						f.write(ext.subdomain)
+						f.write(",")
+						f.write(ext.registered_domain)
 						f.write('\n')
 	f.close()
 			 
