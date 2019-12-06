44461b4df05ce3198ad9c190ec21a872633d2a24
Shehtab Zaman <shehtabzaman@gmail.com>
2019-12-06 14:43:25 -0500

    Added quality check

--- bias_check.py
+++ bias_check.py
@@ -62,8 +62,58 @@ def bias_check(sources):
 
 
 
-def quality_check():
-	print("tbi")
+def quality_check(sources):
+	fd = pd.read_csv("domains_search.csv")
+	topn = fd['domains']
+
+	rval = {}
+	rval['HIGH'] = 0
+	rval['MIXED'] = 0
+	rval['LOW'] = 0
+
+
+	total = 0
+	for each in topn:
+		if (each in sources.keys()):
+			bias = sources[each][0]
+			rval[bias] +=1
+			total +=1
+	# print(rval)
+	
+	vec = ['LOW', 'MIXED', 'HIGH']
+	vals = []
+	data_points = []
+	counter = 0
+	for each in vec:
+		vals.append((each,rval[each]))
+		temp = [counter] * rval[each]
+		counter +=1
+		data_points = data_points + temp
+
+	# kernel = 'gaussian'
+	a,loc,scale = stats.skewnorm.fit(data_points)
+	# print(skew_norm)
+	# data_points = np.array(data_points)[:, np.newaxis]
+
+	# kde = KernelDensity(kernel=kernel, bandwidth=.75).fit(data_points)
+	X_plot = np.linspace(-.5, 3, 1000)[:, np.newaxis]
+	
+	# print(kde.get_params(deep=True))
+	pdf = stats.skewnorm.pdf(X_plot, a=a,loc=loc,scale=scale)
+	# kde = KernelDensity(kernel='cosine', bandwidth=1).fit(data_points)
+	# cos_pdf = kde.score_samples(X_plot)
+	rvs = stats.skewnorm.rvs(size=6000, a=a,loc=loc,scale=scale)
+
+	D,p_val = stats.ks_2samp(rvs, data_points)
+
+	plt.bar([x[0] for x in vals], [x[1]/total for x in vals], width=1)
+	plt.plot(X_plot, pdf , color = 'r', label = "Skew Norm (P-value: "+str(p_val)+" )")
+	# # plt.plot(X_plot[:,0],np.exp(cos_pdf) , color = 'g', label = "Cosine KDE")
+	
+	plt.title("Source Quality of Political Twitter ")
+	plt.legend()
+	plt.show()
+	# plt.savefig("t.png")
 
 def main():
 	f = open("corpus.csv","r")
@@ -77,7 +127,8 @@ def main():
 			sources[ext.domain] = (lines[3], lines[4].strip())
 			# print(ext.domain, lines[3], lines[4])
 		counter +=1
-	bias_check(sources)
+	# bias_check(sources)
+	quality_check(sources)
 
 
 main()
