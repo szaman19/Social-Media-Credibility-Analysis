1927b50585b74142e2c204eb37f1fda6ea368273
Shehtab Zaman <szaman5@titan.cs.binghamton.edu>
2019-12-06 07:16:44 -0500

    Added compressed domain names

--- doc_to_csv.py
+++ doc_to_csv.py
@@ -1,21 +1,19 @@
 import pymongo
-import json
-from datetime import datetime
-import matplotlib.pyplot as plt; plt.rcdefaults()
-import matplotlib.pyplot as plt
-import numpy as np
 import tldextract
 
 def main():
 	myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 	twitter_db = myclient["twitter_data"]
-	collection = twitter_db['streamed_tweets']
+	collection = twitter_db['url_tweets_test']
 	# print(collection.count())
 	f = open("domains.csv", 'w')
 	f.write("domains \n")
 	counter = 0
-	for doc in collection.find():
-		# print(doc.keys())
+	tweets = collection.find()
+	total  = tweets.count()
+	for doc in tweets:
+		counter +=1
+		print("Analyzing: ", counter , " / ", total)
 		if 'delete' not in doc.keys():
 			if 'entities' in doc.keys():
 				ent = doc['entities']
@@ -24,7 +22,6 @@ def main():
 					urls = ent['urls']
 					if(len(urls) > 0):
 						ext = tldextract.extract(urls[0]['expanded_url'])
-						print(ext.domain)
 						f.write(ext.domain)
 						f.write('\n')
 	f.close()
